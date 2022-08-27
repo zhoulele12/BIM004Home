2630340e1c3a3b5fa93677e072c3677aecfcc704
Chengyang Zhou <chengyang.zhou@duke.edu>
2022-08-27 19:53:07 -0400

    added servo test code

---
+++ servo/servo.ino
@@ -0,0 +1,13 @@
+#include <Servo.h>
+
+Servo my_servo;
+void setup() {
+  // put your setup code here, to run once
+  my_servo.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //10 and 60, 60->10 is locking
+  my_servo.write(60);
+}
